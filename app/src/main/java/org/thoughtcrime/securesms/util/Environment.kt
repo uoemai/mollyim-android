a9f3f4c44662c69390694ea1ea8de8e84d47440a
Oscar Mira <valldrac@molly.im>
2022-10-17 21:08:07 +0200

    Add build variant for local dev environment

--- app/src/main/java/org/thoughtcrime/securesms/util/Environment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/util/Environment.kt
@@ -4,4 +4,5 @@ import org.thoughtcrime.securesms.BuildConfig
 
 object Environment {
   const val IS_STAGING: Boolean = BuildConfig.BUILD_ENVIRONMENT_TYPE == "Staging"
+  const val IS_DEV: Boolean = BuildConfig.BUILD_ENVIRONMENT_TYPE == "Dev" || BuildConfig.BUILD_VARIANT_TYPE == "Instrumentation"
 }
